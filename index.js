d3cbbb85eba224d5393223c4ebf5652cbd1b9438
Sam Bateman <sam@bateman.io>
2017-07-13 17:54:27 -0600

    add env file

--- index.js
+++ index.js
@@ -1,7 +1,8 @@
 var express = require('express');
 var app = express();
 
-var port = process.env.PORT || 5000;
+
+app.set('port', (process.env.PORT || 5000));
 
 app.use(express.static(__dirname + '/public'));
 
@@ -10,7 +11,7 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 
-app.listen(port, function() {
+app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
 
