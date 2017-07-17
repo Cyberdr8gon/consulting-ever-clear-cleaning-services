89e198f40e308143c008ba31427b413633e695be
Sam Bateman <sam@bateman.io>
2017-07-17 14:03:21 -0600

    auto-deploy:

--- deploy.sh
+++ deploy.sh
@@ -3,11 +3,7 @@ set -e
 ./build.sh
 git add .
 git -c color.status=false status \
-  | sed -n -r -e '1,/Changes to be committed:/ d' \
-              -e '1,1 d' \
-                          -e '/^Untracked files:/,$ d' \
-                                      -e 's/^\s*//' \
-                                                  -e '/./p' \
-                                                  | git commit -F -
+  | sed -n -r -e '1,/Changes to be committed:/ d' \ -e '1,1 d' \ -e '/^Untracked files:/,$ d' \ -e 's/^\s*//' \ -e '/./p' \
+  | git commit -F -
 git push
 git push heroku master
