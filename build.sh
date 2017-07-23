a0eeb2fdffe91d073642389df0ec906929073afe
Sam Bateman <sam@bateman.io>
2017-07-22 22:15:37 -0600

    moved all tooling to npm scripts and added blog. Must add styles and begin backend component

--- build.sh
+++
@@ -1,4 +0,0 @@
-cd ./front-end-staging/company-site
-hugo -t=company-theme
-cd ../..
-cp -r ./front-end-staging/company-site/public/* ./public
--- deploy.sh
+++
@@ -1,8 +0,0 @@
-set -e
-
-./build.sh
-git add .
-DATE=`date +%Y-%m-%d:%H:%M:%S`
-git commit -m "auto-deploy:$DATE"
-git push
-git push heroku master
