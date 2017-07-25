db4b6997911a37d3f50ffe5b0b54f303b6209ae6
Sam Bateman <sam@bateman.io>
2017-07-25 09:56:42 -0600

    forgot to save work

--- index.js
+++ index.js
@@ -12,6 +12,24 @@ app.set('views', __dirname + '/views');
 app.set('view engine', 'ejs');
 
 
+app.post("/api/load", function(request, response) {
+  console.log("Client Load");
+  console.log(request);
+  response.json({ message: '/api/load Successful' });
+});
+
+app.post("/api/bounce", function(request, response) {
+  console.log("Client Didn't Bounce");
+  console.log(request);
+  response.json({ message: '/api/bounce Successful' });
+});
+
+app.post("/api/convert", function(request, response) {
+  console.log("Client Likely Converted");
+  console.log(request);
+  response.json({ message: '/api/convert Successful' });
+});
+
 app.listen(app.get('port'), function() {
   console.log('Node app is running on port', app.get('port'));
 });
